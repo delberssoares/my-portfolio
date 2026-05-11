51bdb1a7268fe6fa41bfeff71e7ec5c3e382032d
delberss <delberss@hotmail.com>
2026-05-11 19:48:44 -0300

    Updating links - Frontend

--- src/pages/Home/sections/ProjectsSection/ProjectsSection.tsx
+++ src/pages/Home/sections/ProjectsSection/ProjectsSection.tsx
@@ -14,8 +14,8 @@ const projects: ProjectCardProps[] = [
         srcImages: ["/images/preview.png"],
         description: "A modern and responsive portfolio built with React, TypeScript, and Material UI, showcasing my projects, skills, and contact information.",
         technologies: "React, TypeScript, Material UI",
-        websiteURL: "https://delber-portfolio.vercel.app/",
-        codeURL: "https://github.com/delberss/my-portfolio",
+        websiteURL: "https://my-portfolio-delberss.vercel.app/",
+        codeURL: "https://github.com/delberssoares/my-portfolio",
         hasLivePreview: true,
     },
     {
@@ -23,8 +23,8 @@ const projects: ProjectCardProps[] = [
         srcImages: ["/images/cinema.png"],
         description: "A modern and responsive platform for selling movie tickets, developed with React, TypeScript, Zustand, and Material UI.",
         technologies: "React, TypeScript, Zustand, Material UI",
-        websiteURL: "https://cinema-delberss.vercel.app/",
-        codeURL: "https://github.com/delberss/cinema",
+        websiteURL: "https://my-cinema-delberss.vercel.app/",
+        codeURL: "https://github.com/delberssoares/cinema",
         hasLivePreview: true,
     },
     {
@@ -32,10 +32,10 @@ const projects: ProjectCardProps[] = [
         srcImages: ["/images/text-analyzer.png"],
         description: "A simple, modern, and responsive platform for text analysis, built with React, TypeScript, Styled Components, and Material UI, and integrated with a Python backend for data processing.",
         technologies: "React, TypeScript, Python, FastAPI, Uvicorn,Material UI",
-        websiteURL: "https://dss-text-analyzer-frontend.vercel.app/",
+        websiteURL: "https://text-analyzer-frontend.vercel.app/",
         codeURL: {
-            frontend: "https://github.com/delberss/dss-text-analyzer-frontend",
-            backend: "https://github.com/delberss/dss-text-analyzer-backend"
+            frontend: "https://github.com/delberssoares/text-analyzer-frontend",
+            backend: "https://github.com/delberssoares/text-analyzer-backend"
         },
 
         hasLivePreview: true,
@@ -45,8 +45,8 @@ const projects: ProjectCardProps[] = [
         srcImages: ["/images/dss-store.png"],
         description: "A modern marketplace frontend built with React, featuring user registration, product listing, cart management with Zustand, and a seamless checkout process with contact, shipping details, and Stripe credit card payments.",
         technologies: "React, TypeScript, Node, Express, Stripe API, Zustand",
-        websiteURL: "https://github.com/delberss/marketplace/blob/master/README.md",
-        codeURL: "https://github.com/delberss/marketplace",
+        websiteURL: "https://github.com/delberssoares/marketplace/blob/main/README.md",
+        codeURL: "https://github.com/delberssoares/marketplace",
         hasLivePreview: false
     },
     {
